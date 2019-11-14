bc757bc321d4b1afbb3c6145f9c0ee2a9539ff12
Xealeph <51519267+Xealeph@users.noreply.github.com>
2019-11-15 00:25:26 +0100

    updated (15.11.2019)
    might have some bugs regarding file formats (e.g. image.pngformat)
    at least it should work again

--- yiff_image_scraper.py
+++ yiff_image_scraper.py
@@ -24,6 +24,8 @@ if not os.path.isdir(".\\Images\\"):
 
 def downloadImages(url, urlCounter):
     linkList = []
+    imgContainerUrls = []
+    imageCounter = 0
 
     #Gets the Patreon Author's number. Fails if link is shorter than https://yiff.party/patreon/1.
     #Also Creates a directory for the images.
@@ -41,34 +43,55 @@ def downloadImages(url, urlCounter):
     response = requests.get(url)
     soup = bs(response.text, "html.parser")
 
-    containers = soup.find_all('div', {'class':'card-action'})
 
-    #Checks if there are any images and returns an error if not. Also skipps the url.
-    try:
-        containers[0]
-    except IndexError:
-        print("\nCould not find Images. The cause might be a invalid url or there just aren't any Images")
-        print("Skipping url: " + url + "\n")
-        print("============" + str(urlCounter) + "/" + str(amountOfLinks) + "===============\n")
-        return
+    #searches for the highest page number
+    lastPage = soup.find_all('a', {'class':'btn pag-btn'})
+    lastPage = int(lastPage[1]["data-pag"])
 
-    #Searches for Image-Boxes.
-    for container in containers:
-        shortLink = container.a['href']
-        linkList.append("https://yiff.party" + shortLink)
+    #new url request
+    newUrl = "https://yiff.party/render_posts?s=patreon&c=" + patreonAuthor + "&p="
 
-    #Loops through the Image Urls amd downloads them.
-    for url in linkList:
-        imageName = url.split("/")[6]
-        #print("Downloading " + name)           #Shows the name of the current downloading image
-        r = requests.get(url, stream=True)
-        if r.status_code == 200:
-            with open(".\\Images\\" + patreonAuthor + "\\" + imageName, 'wb') as f:
-                for chunk in r:
-                    f.write(chunk)
+    for i in range(lastPage-1):
+        imgContainerUrls.append(newUrl + str(i+1)) #appends the page number to the url
+
+    for containerUrl in imgContainerUrls:
+        response = requests.get(containerUrl)
+        soup = bs(response.text, "html.parser")
+
+        containers = soup.find_all('div', {'class': 'card-action'})
+        imageCounter += len(containers)
+
+        #Checks if there are any images and returns an error if not. Also skipps the url.
+        try:
+            containers[0]
+        except IndexError:
+            print("\nCould not find Images. The cause might be a invalid url or there just aren't any Images")
+            print("Skipping url: " + url + "\n")
+            print("============" + str(urlCounter) + "/" + str(amountOfLinks) + "===============\n")
+            return
+
+        #Searches for Image-Boxes.
+        for container in containers:
+            shortLink = container.a['href']
+            linkList.append(shortLink)
+
+        #Loops through the Image Urls amd downloads them.
+        for urlI in linkList:
+            
+            longUrl = "https://yiff.party" + urlI
+
+            imageName = urlI.split("/")[len(urlI.split("/"))-1]
+            print("Downloading " + imageName)           #Shows the name of the current downloading image
+            r = requests.get(longUrl, stream=True)
+            if r.status_code == 200:
+                with open(".\\Images\\" + patreonAuthor + "\\" + imageName, 'wb') as f:
+                    for chunk in r:
+                        f.write(chunk)
+            else:
+                print("beep -- file skipped: " + longUrl)
 
     #Just a finishing message.
-    print("\nSuccessfully downloaded " + str(len(containers)) + " Images!\n")
+    print("\nSuccessfully downloaded " + str(len(imageCounter)) + " Images!\n")
     print("============" + str(urlCounter) + "/" + str(amountOfLinks) + "===============\n")
 
 #Loops through all Yiff.party-Urls and downloads the images.
